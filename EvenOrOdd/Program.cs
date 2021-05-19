e5b8d7e6e3de6f72387d51499ef70839f95b40ba
John Atkins <jdatkins@tql.com>
2021-05-19 11:27:30 -0400

    Add project files.

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answerStr;
+            int answer;
+
+            Console.Write("Enter an integer: ");
+
+            while (1 == 1)
+            {
+                answerStr = Console.ReadLine();
+                if (!int.TryParse(answerStr, out answer) && answerStr != "q" && answerStr != "Q")
+                {
+                    Console.WriteLine("Invalid input. Enter an integer, or enter 'q' to quit: ");
+                    continue;
+                }
+                else if (answerStr == "q" || answerStr == "Q")
+                {
+                    return;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            while (1 == 1)
+            {
+                var evenNumber = answer % 2 == 0;
+                if (evenNumber)
+                {
+                    Console.WriteLine($"{answer} is even.");
+                }
+                else
+                {
+                    Console.WriteLine($"{answer} is odd.");
+                }
+
+                Console.WriteLine("Enter another integer, or enter 'q' to quit.");
+
+                while (1 == 1)
+                {
+                    answerStr = Console.ReadLine();
+                    if (!int.TryParse(answerStr, out answer) && answerStr != "q" && answerStr != "Q")
+                    {
+                        Console.WriteLine("Invalid input. Enter an integer, or enter 'q' to quit: ");
+                        continue;
+                    }
+                    else if (answerStr == "q" || answerStr == "Q")
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
